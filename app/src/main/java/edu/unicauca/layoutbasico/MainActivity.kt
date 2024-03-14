caed60678aac4448f9369269e8509032ee2a465b
Cristhian Figueroa <cfigmart@users.noreply.github.com>
2024-03-14 18:44:33 -0500

    Agrego imagenes y creo TarjetaImagenTexto

--- app/src/main/java/edu/unicauca/layoutbasico/MainActivity.kt
+++ app/src/main/java/edu/unicauca/layoutbasico/MainActivity.kt
@@ -3,6 +3,8 @@ package edu.unicauca.layoutbasico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.icons.Icons
@@ -15,6 +17,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,6 +70,29 @@ fun BarraBuscar(modifier:Modifier = Modifier){
 fun PreviewBarraBuscar(){
     BarraBuscar()
 }
+
+@Composable
+fun TarjetaImagenTexto(){
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.ab1_inversions),
+            contentDescription = null)
+        Text(text = stringResource(id = R.string.inversions_txt))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTarjetaImagenTexto(){
+    TarjetaImagenTexto()
+}
+
+
+
+
+
+
+
 
 
 
