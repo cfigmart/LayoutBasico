d430030781d4c1a2a0b1fe4f3ddfd2a5146af8a4
Cristhian Figueroa <cfigmart@users.noreply.github.com>
2024-03-14 19:21:02 -0500

    Creo TarjetaImagenTexto personalizable y agrego Icono de aplicación.

--- app/src/main/java/edu/unicauca/layoutbasico/MainActivity.kt
+++ app/src/main/java/edu/unicauca/layoutbasico/MainActivity.kt
@@ -3,10 +3,17 @@ package edu.unicauca.layoutbasico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -16,7 +23,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,10 +40,10 @@ class MainActivity : ComponentActivity() {
             LayoutBasicoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                   // modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BarraBuscar()
+                    PantallaPrincipal()
                 }
             }
         }
@@ -72,19 +82,53 @@ fun PreviewBarraBuscar(){
 }
 
 @Composable
-fun TarjetaImagenTexto(){
-    Column {
+fun TarjetaImagenTexto(
+    @StringRes textoId: Int,
+    @DrawableRes imagenId: Int,
+    modifier:Modifier = Modifier){
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
         Image(
-            painter = painterResource(id = R.drawable.ab1_inversions),
-            contentDescription = null)
-        Text(text = stringResource(id = R.string.inversions_txt))
+            painter = painterResource(id = imagenId),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .size(88.dp)
+                .clip(CircleShape)
+
+
+
+            )
+        Text(text = stringResource(id = textoId),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium)
     }
 }
 
 @Preview
 @Composable
 fun PreviewTarjetaImagenTexto(){
-    TarjetaImagenTexto()
+    TarjetaImagenTexto(
+        textoId = R.string.inversions_txt,
+        imagenId = R.drawable.ab1_inversions
+    )
+}
+
+@Composable
+fun PantallaPrincipal(modifier:Modifier = Modifier){
+    Column (verticalArrangement = Arrangement.Top) {
+        BarraBuscar()
+        TarjetaImagenTexto(
+            textoId = R.string.quick_yoga_txt,
+            imagenId = R.drawable.ab2_quick_yoga
+
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPantallaPrincipal(){
+    PantallaPrincipal()
 }
 
 
